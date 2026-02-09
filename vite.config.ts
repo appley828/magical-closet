b5c8f4d28e97c4d55b71136e75be35ebfc4e35d9
APPLE <apple@Apple-Hsu.local>
2026-02-09 18:29:58 +0800

    fix: 修正路徑與語法錯誤

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/magical-closet/', // 確保路徑與你的 GitHub 專案名稱一致
+  base: '/magical-closet/', 
   plugins: [
     react(),
     tailwindcss()
